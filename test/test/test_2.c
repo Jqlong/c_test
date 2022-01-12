cc0801f3803239ed1376a4db3a1adeafb095312f
JqlongC <2235715529@qq.com>
2022-01-12 21:37:56 +0800

    c_test

---
+++ test/test/test_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c' };
+	char arr3[] = { 'a','b','c','\0' };
+	int len1 = strlen(arr1);
+	int len2 = strlen(arr2);
+	int len3 = strlen(arr3);
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+	printf("%s\n", arr3);
+	printf("%d\n", len1);  //3
+	printf("%d\n", len2);  //15
+	printf("%d\n", len3);  //3
+	return 0;
+
+}
