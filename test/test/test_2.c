ed1771ad2ffd5d9aac3674870c76028fa9cc2e90
JqlongC <2235715529@qq.com>
2022-01-14 22:16:29 +0800

    test_2.c

--- test/test/test_2.c
+++ test/test/test_2.c
@@ -1,24 +1,40 @@
- #include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
 #include <string.h>
 int main() {
+//
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c' };
+//	char arr3[] = { 'a','b','c','\0' };
+//	int len1 = strlen(arr1);
+//	int len2 = strlen(arr2);
+//	int len3 = strlen(arr3);
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	printf("%s\n", arr3);
+//	printf("%d\n", len1);  //3
+//	printf("%d\n", len2);  //15
+//	printf("%d\n", len3);  //3
+//
+//	printf("c:\\test\n");
+//	printf("\a\a\a");
+//
+//	printf("%c\n", '\'');
+//	return 0;
 
-	char arr1[] = "abc";
-	char arr2[] = { 'a','b','c' };
-	char arr3[] = { 'a','b','c','\0' };
-	int len1 = strlen(arr1);
-	int len2 = strlen(arr2);
-	int len3 = strlen(arr3);
-	printf("%s\n", arr1);
-	printf("%s\n", arr2);
-	printf("%s\n", arr3);
-	printf("%d\n", len1);  //3
-	printf("%d\n", len2);  //15
-	printf("%d\n", len3);  //3
+	int input = 0;
 
-	printf("c:\\test\n");
-	printf("\a\a\a");
+	printf("你要加入bit吗？>");
+	printf("你要好好学习吗：>\n");
 
-	printf("%c\n", '\'');   
+	scanf("%d", &input);
+	if (input == 0) {
+		printf("拿个offer\n");
+	}
+	else {
+		printf("回家卖红薯\n");
+	}
 	return 0;
 
 }
